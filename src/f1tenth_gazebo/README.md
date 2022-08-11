615706adbd6ad3d3e4efaa11dd75761e32fc2ca2
KhalidOwlWalid <khalidowlwalid@gmail.com>
2022-08-11 06:10:23 +0100

    Create f1tenth_gazebo package

---
+++ src/f1tenth_gazebo/README.md
@@ -0,0 +1,7 @@
+Some of the things I find interesting:
+
+### Urdf to sdf
+https://automaticaddison.com/how-to-convert-a-xacro-file-to-urdf-and-then-to-sdf/ <br />
+
+### Set GAZEBO_ROS_PATHS 
+https://github.com/ros-simulation/gazebo_ros_pkgs/wiki/ROS-2-Migration:-Gazebo-ROS-Paths#env-hooks
---
+++ src/f1tenth_gazebo/launch/f1tenth.launch.py
@@ -0,0 +1,55 @@
+# Copyright 2019 Louise Poubel
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Launch Gazebo with a world that has Dolly, as well as the follow node."""
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_f1tenth_gazebo = get_package_share_directory('f1tenth_gazebo')
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
+    
+    urdf_file_path = os.path.join(get_package_share_directory('f1tenth_gazebo'), 'models', 'f1tenth', 'model.urdf')
+    
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        output='screen',
+        arguments=['-entity', 'f1tenth', '-file', urdf_file_path],
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+          'world',
+          default_value=[os.path.join(pkg_f1tenth_gazebo, 'worlds', 'f1tenth_empty.world'), ''],
+          description='SDF world file'),
+        gazebo,
+        spawn_entity,
+    ])
