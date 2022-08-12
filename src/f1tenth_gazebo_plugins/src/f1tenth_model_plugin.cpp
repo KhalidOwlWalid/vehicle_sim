29b665a57793f666a28ee298dc2c22d64e066833
KhalidOwlWalid <khalidowlwalid@gmail.com>
2022-08-12 21:11:25 +0100

    Add f1tenth model plugin

---
+++ src/f1tenth_gazebo_plugins/src/f1tenth_model_plugin.cpp
@@ -0,0 +1,154 @@
+// Copyright (c) 2013, Markus Bader <markus.bader@tuwien.ac.at>
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the company nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <gazebo/common/Events.hh>
+#include <gazebo/common/Time.hh>
+#include <gazebo/physics/Joint.hh>
+#include <gazebo/physics/Model.hh>
+#include <gazebo/physics/World.hh>
+#include <f1tenth_gazebo_plugins/f1tenth_model_plugin.hpp>
+#include <gazebo_ros/conversions/builtin_interfaces.hpp>
+#include <gazebo_ros/node.hpp>
+#ifdef IGN_PROFILER_ENABLE
+#include <ignition/common/Profiler.hh>
+#endif
+#include <rclcpp/rclcpp.hpp>
+#include <ackermann_msgs/msg/ackermann_drive_stamped.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace gazebo_plugins
+{
+class GazeboRosF1TenthModelPrivate
+{
+public:
+  /// Callback to be called at every simulation iteration.
+  /// \param[in] info Updated simulation info.
+  void OnUpdate(const gazebo::common::UpdateInfo & info);
+
+  /// A pointer to the GazeboROS node.
+  gazebo_ros::Node::SharedPtr ros_node_;
+
+  /// Joint state publisher.
+  rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_topic_pub_;
+
+  /// Period in seconds
+  double update_period_;
+
+  /// Keep last time an update was published
+  gazebo::common::Time last_update_time_;
+
+  /// Pointer to the update event connection.
+  gazebo::event::ConnectionPtr update_connection_;
+};
+
+GazeboRosF1TenthModel::GazeboRosF1TenthModel()
+: impl_(std::make_unique<GazeboRosF1TenthModelPrivate>())
+{
+}
+
+GazeboRosF1TenthModel::~GazeboRosF1TenthModel()
+{
+}
+
+void GazeboRosF1TenthModel::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf)
+{
+  // ROS node
+  impl_->ros_node_ = gazebo_ros::Node::Get(sdf);
+
+  // Get QoS profiles
+  const gazebo_ros::QoS & qos = impl_->ros_node_->get_qos();
+
+  // Joints
+  std::string drive_topic_ = "cmd";
+  if (!sdf->HasElement("drive_topic")) {
+    RCLCPP_INFO(impl_->ros_node_->get_logger(), "Missing <drive_topic> tag, defaults to %s", drive_topic_.c_str());
+  } else {
+    drive_topic_ = sdf->GetElement("drive_topic")->Get<std::string>();
+  }
+
+  // Update rate
+  double update_rate = 100.0;
+  if (!sdf->HasElement("update_rate")) {
+    RCLCPP_INFO(
+      impl_->ros_node_->get_logger(), "Missing <update_rate>, defaults to %f", update_rate);
+  } else {
+    update_rate = sdf->GetElement("update_rate")->Get<double>();
+  }
+
+  if (update_rate > 0.0) {
+    impl_->update_period_ = 1.0 / update_rate;
+  } else {
+    impl_->update_period_ = 0.0;
+  }
+
+  impl_->last_update_time_ = model->GetWorld()->SimTime();
+
+  // Joint state publisher
+  impl_->drive_topic_pub_ = impl_->ros_node_->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(
+    drive_topic_, qos.get_publisher_qos(drive_topic_, rclcpp::QoS(1000)));
+
+  // Callback on every iteration
+  impl_->update_connection_ = gazebo::event::Events::ConnectWorldUpdateBegin(
+    std::bind(&GazeboRosF1TenthModelPrivate::OnUpdate, impl_.get(), std::placeholders::_1));
+}
+
+void GazeboRosF1TenthModelPrivate::OnUpdate(const gazebo::common::UpdateInfo & info)
+{
+#ifdef IGN_PROFILER_ENABLE
+  IGN_PROFILE("GazeboRosF1TenthModelPrivate::OnUpdate");
+#endif
+  gazebo::common::Time current_time = info.simTime;
+
+  // If the world is reset, for example
+  if (current_time < last_update_time_) {
+    RCLCPP_INFO(ros_node_->get_logger(), "Negative sim time difference detected.");
+    last_update_time_ = current_time;
+  }
+
+  // Check period
+  double seconds_since_last_update = (current_time - last_update_time_).Double();
+
+  if (seconds_since_last_update < update_period_) {
+    return;
+  }
+
+  std::cout << "Test" << std::endl;
+
+  // Update time
+  last_update_time_ = current_time;
+}
+
+GZ_REGISTER_MODEL_PLUGIN(GazeboRosF1TenthModel)
+}  // namespace gazebo_plugins
